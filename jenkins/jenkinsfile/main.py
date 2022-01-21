d9fae9278783a8264e41513ecacdffe4954db20e
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-21 13:03:47 +0530

    Add files via upload

---
+++ jenkins/jenkinsfile/main.py
@@ -0,0 +1,8 @@
+
+# import pandas lib as pd
+import pandas as pd
+ 
+# read by default 1st sheet of an excel file
+dataframe1 = pd.read_excel('Individual_parameter_coverage.xlsx')
+ 
+print(dataframe1)
