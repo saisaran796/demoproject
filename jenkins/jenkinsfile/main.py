89974077b757ecd1bca2a9c23351dac19056751e
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-21 14:28:48 +0530

    Delete main.py

--- jenkins/jenkinsfile/main.py
+++
@@ -1,8 +0,0 @@
-
-# import pandas lib as pd
-import pandas as pd
- 
-# read by default 1st sheet of an excel file
-dataframe1 = pd.read_excel('Individual_parameter_coverage.xlsx')
- 
-print(dataframe1)
