a3ba1372e9ce1e111758cc9c8bb5d5d3c65f0fd4
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-21 13:33:25 +0530

    Update scripted_pipeline_ex_2.sh

--- jenkins/script/scripted_pipeline_ex_2.sh
+++ jenkins/script/scripted_pipeline_ex_2.sh
@@ -7,7 +7,6 @@ cat <<EOF | python3 -
 import pandas as pd
  
 # read by default 1st sheet of an excel file
-dataframe1 = pd.read_excel('Individual_parameter_coverage.xlsx')
- 
-print(dataframe1)
+df = pd.read_excel (r'./jenkins/Individual_parameter_coverage.xlsx')
+print(df)
 EOF
