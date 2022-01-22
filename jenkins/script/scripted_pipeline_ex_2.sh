7f7231e58ffa44f1f311c510149685c890e5a8ec
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-22 13:47:52 +0530

    Update scripted_pipeline_ex_2.sh

--- jenkins/script/scripted_pipeline_ex_2.sh
+++ jenkins/script/scripted_pipeline_ex_2.sh
@@ -12,39 +12,61 @@ import requests
 df1 = pd.read_excel (r'./jenkins/Individual_parameter_coverage.xlsx')
 print(df1)
 xml_data = open(r'./jenkins/config.xml').read()  # Read file
-# convert an xml file into a list of dictionaries.
-# each element of the list is a dictionary of all the (key: attribute) and (tag: text) pairs found in
-# a path descending through XML tag layers before terminating at a tag with no children.
-def parse_xml(root):
-parsed_data = {}
-# get data from attributes in the current tag
-for key in root.keys():
-parsed_data[key] = root.attrib.get(key)
-# check if current level has children
-if not root.getchildren():
-return [parsed_data]
-else:
-child_list = []
-has_children = False
-# for each child, get data stored in the text
-for child in root.getchildren():
-if child.text:
-parsed_data[child.tag] = child.text
-if child.getchildren():
-has_children = True
-# if any child has children, continue the recursive parsing
-if has_children:
-for child in root.getchildren():
-child_data = parse_xml(child)
-# for each parsed child, take it's dictionary of values and add it the already-parsed data from
-# higher level tags. One row of data per child of the current tag
-for child_row_dict in child_data:
-# combine two dictionaries and append to list of row-dictionaries
-child_list.append({**parsed_data, **child_row_dict})
-return child_list
-else:
-return [parsed_data]
-xml_data = open('path/to/xml.xml').read()
-root = et.XML(xml_data)
-parsed_df = pd.DataFrame(data=parse_xml(root))
+def xml2df(xml_data, leave_out=[]):
+
+#Initiates the tree Ex: <user-agents>
+
+tree = ET.parse(xml_data)
+
+#Starts the root of the tree Ex: <user-agent>
+
+root = tree.getroot()
+
+#This is our record list which we will convert into a dataframe
+
+all_records = []
+
+#Subchildren tags will be parsed and appended here
+
+headers = []
+
+#Begin looping through our root tree
+
+for i, child in enumerate(root):
+
+record = []
+
+#iterate through the subchildren to user-agent, Ex: ID, String, Description
+
+for subchild in child:
+
+#This is my modification, if a record that we don't want to include in the data framework
+
+#is detected just skeep it
+
+if subchild.text in leave_out:
+
+break
+
+#Extract the text and append it to our record list
+
+record.append(subchild.text)
+
+#Check the header list to see if the subchild tag <id>, <string>...
+
+#is in our headers field. If not append it. This will be used for our headers
+
+if subchild.tag not in headers:
+
+headers.append(subchild.tag)
+
+#Append this record to all_records, only if the record is not empty
+
+if record != []:
+
+all_records.append(record)
+
+#Finally, return our Pandas dataframe with headers in the column
+
+return pd.DataFrame(all_records, columns=headers)
 EOF
