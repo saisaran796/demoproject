0013b96f0c2cfffe84192e0d287e7125bdd7bfb3
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-22 13:40:55 +0530

    Update scripted_pipeline_ex_2.sh

--- jenkins/script/scripted_pipeline_ex_2.sh
+++ jenkins/script/scripted_pipeline_ex_2.sh
@@ -13,19 +13,13 @@ df1 = pd.read_excel (r'./jenkins/Individual_parameter_coverage.xlsx')
 print(df1)
 xml_data = open(r'./jenkins/config.xml').read()  # Read file
 def xml2df(xml_data):
-    tree = ET.parse(xml_data)
-    root = list(list(tree.getroot())[0])[1]
-    all_records = []
-    headers = []
-    for i, child in enumerate(root):
-        if i < 2:
-            headers.append(list(child)[0].text)
-            continue
-        record = []
-        for subchild in child:
-            record.append(subchild.text)
-        all_records.append(record)
-    return pd.DataFrame(all_records, columns=headers)
-
-tt = xml2df(xml_data)
+tree = ET.parse(xml_data)
+root = tree.getroot()
+all_records = []
+for i, child in enumerate(root):
+record = {}
+for subchild in child:
+record[subchild.tag] = subchild.text
+all_records.append(record)
+return pd.DataFrame(all_records)
 EOF
