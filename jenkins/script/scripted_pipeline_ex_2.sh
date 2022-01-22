c9b02760b7cfac8e992f059ea7b5c3f0e956d2ad
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-22 11:43:16 +0530

    Update scripted_pipeline_ex_2.sh

--- jenkins/script/scripted_pipeline_ex_2.sh
+++ jenkins/script/scripted_pipeline_ex_2.sh
@@ -8,7 +8,7 @@ import pandas as pd
  
 # read by default 1st sheet of an excel file
 df1 = pd.read_excel (r'./jenkins/Individual_parameter_coverage.xlsx')
-print(df)
+print(df1)
 def alert_read_text(df1, alert_status=None):
     if (alert_status is None):
         print 'Warning: A column name with the alerts must be specified'
