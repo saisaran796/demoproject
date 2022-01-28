25bac2d7f3a2e7c67e07a159b0261326a5a1aa5f
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-28 14:17:29 +0530

    Update main.py

--- jenkins/main.py
+++ jenkins/main.py
@@ -1,6 +1,12 @@
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import PatternFill, Side, borders, Font, Alignment
 import xml.etree.ElementTree as ET
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import pandas as pd
 
 
 def psm_xml():
@@ -11,35 +17,44 @@ def psm_xml():
     return xml_parameter
 
 
-def txt_extract() :
-    text_file =r"./jenkins/Psm_coverage_psm_dev.txt"
-    words= {}
-    with open(text_file,"r") as file:
+xml_dataframe = psm_xml()
+
+
+df_xml = pd.DataFrame(xml_dataframe)
+
+# print(df_xml)
+
+
+def txt_extract():
+    text_file = r"./jenkins/Psm_coverage_psm_dev.txt"
+    words = {}
+    with open(text_file, "r") as file:
         data = file.readlines()
-        try :
-            for index,line in enumerate(data):
-                if index > 5 :
-                    keys =line.split(" ")[0]
-                    value=float(line.split(" ")[-1])
-                    if keys not in words.keys() :
+        try:
+            for index, line in enumerate(data):
+                if index > 5 and line.split(" ")[0] != "--":
+                    keys = line.split(" ")[0]
+                    value = float(line.split(" ")[-1].strip().replace(",", "."))
+                    if keys not in words.keys():
                         words[keys] = []
                         words[keys].append(value)
                     else:
                         words[keys].append(value)
-        except :
+        except:
             pass
-
     text_data = {}
     for coverage_key in words:
         coverage_value = sum(words[coverage_key])*100/len(words[coverage_key])
         text_data[coverage_key] = coverage_value
 
-    # print("txt Data",(text_data))
     return text_data
 
 
-a = txt_extract()
-print(a)
+txt_dataframe = txt_extract()
+df_txt = pd.DataFrame.from_dict(txt_dataframe, orient='index')
+print(df_txt)
+
+
 def xl_parameters():
     wb = load_workbook(r"./jenkins/Individual_parameter_coverage.xlsx")
     ws = wb["Sheet1"]
@@ -58,35 +73,33 @@ def xl_parameters():
         s = ws.cell(row=row, column=start_col)
         parameter_name[s.value] = ws.cell(row=row, column=start_col+1).value
     del parameter_name[None]
-
-    # print("xl Data",parameter_name)
     return parameter_name
+
+
+excel_dataframe = xl_parameters()
+df_excel = pd.DataFrame.from_dict(excel_dataframe, orient='index')
+print(df_excel)
 
 
 def missing_parameter():
     missing_parameters = []
-    name = "Missing Parameters"
     excel = xl_parameters()
-    xfj = psm_xml()
+    xml = psm_xml()
     for i in excel.keys():
-        if i not in xfj:
+        if i not in xml:
             missing_parameters.append(i)
-    xl_writing(name, missing_parameters)
+    return missing_parameters
 
 
 def coverage_results():
-    # diff = DeepDiff (xl_parameters(), txt_extract())
-    # print(diff)
-    sheet_name = "Coverage Results"
+
     result = []
     for i in txt_extract().keys():
         if i in xl_parameters().keys():
             if txt_extract()[i] > xl_parameters()[i]:
                 result.append((i, xl_parameters()[i], txt_extract()[i]))
 
-    if len(txt_extract()) > len(result):
-        return False
-    xl_writing(sheet_name, result)
+    return result
 
 
 def xl_writing(sheet_name, values):
@@ -107,17 +120,10 @@ def xl_writing(sheet_name, values):
     ws2['B2'].alignment = Alignment(horizontal='center', vertical='center')
 
     ws2['B3'] = "Parameter Name"
-    ws2['B3'].alignment = Alignment(horizontal='center', vertical='center')
     ws2['C3'] = "Coverage Ratio(%)"
-    ws2['C3'].alignment = Alignment(horizontal='center', vertical='center')
-    ws2['B3'].fill = PatternFill("solid", start_color="96be25")
-    ws2['B3'].font = Font(color="000000", size="11", bold=True)
-    ws2['C3'].fill = PatternFill("solid", start_color="96be25")
-    ws2['C3'].font = Font(color="000000", size="11", bold=True)
 
     if sheet_name == "Missing Parameters":
         ws2.merge_cells("B2:C2")
-        ws2['C2'].border = border
         s = values
         for row in range(0, len(s)):
             for col in range(1, 2):
@@ -127,16 +133,6 @@ def xl_writing(sheet_name, values):
         ws2.merge_cells("B2:D2")
         ws2.merge_cells("C3:D3")
         ws2.merge_cells("B3:B4")
-        ws2["B2"].border = border
-        ws2['C2'].border = border
-        ws2['B4'].border = border
-        ws2['D2'].border = border
-        ws2['D3'].border = border
-        ws2['D4'].border = border
-        ws2["C4"].value = "Actual"
-        ws2['C4'].fill = PatternFill("solid", start_color="96be25")
-        ws2["D4"].value = "Result"
-        ws2['D4'].fill = PatternFill("solid", start_color="96be25")
 
         s = values
 
@@ -146,17 +142,67 @@ def xl_writing(sheet_name, values):
                 ws2[chr(67) + str(row + 5)] = s[row][1]
                 ws2[chr(68) + str(row + 5)] = s[row][2]
 
-    ws2.column_dimensions["B"].width, ws2.column_dimensions["C"].width,ws2.column_dimensions["D"].width = 42, 18, 15
-    # print(ws2.max_row,ws2.max_column)
+    ws2.column_dimensions["B"].width, ws2.column_dimensions["C"].width, ws2.column_dimensions["D"].width = 42, 18, 15
 
     for row in range(1, ws2.max_row+1):
         for col in range(1, ws2.max_column+1):
             if ws2.cell(row=row, column=col).value:
                 ws2.cell(row=row, column=col).border = border
 
-    ws2.sheet_view.showGridLines = False
-    # wb2.save(r"C:/Users/saran/Desktop/code/Result_Parameters.xlsx")
+    wb2.save(r"./jenkins/Result_Parameters.xlsx")
 
 
-coverage_results()
-# missing_parameter()
+def send_email():
+    fromaddr = "kvedasai172@gmail.com"
+    toaddr = "kvedasai172@gmail.com"
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+
+    msg['To'] = toaddr
+
+    msg['Subject'] = "Subject of the Mail"
+
+    body = "Body_of_the_mail"
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    filename = "Individual parameter coverage - Template.xlsx"
+    attachment = open(r"./jenkins/Individual parameter coverage - Template.xlsx", "rb")
+
+    p = MIMEBase('application', 'octet-stream')
+
+    p.set_payload(attachment.read())
+
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    s.starttls()
+
+    s.login(fromaddr, "VedaK172@")
+
+    text = msg.as_string()
+
+    s.sendmail(fromaddr, toaddr, text)
+
+    s.quit()
+
+
+if len(missing_parameter()) != 0:
+    name = "Missing Parameters"
+    result = missing_parameter()
+    xl_writing(name, result)
+    # send_email()
+else:
+    name = "Coverage Results"
+    result = coverage_results()
+    xl_writing(name, result)
+    if len(txt_extract()) > len(result):
+        pass
+        # send_email()
