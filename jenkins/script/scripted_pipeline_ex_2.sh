ddd3111a74b3ac4b06bb879b6795faa901e7f315
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-28 10:40:33 +0530

    Update scripted_pipeline_ex_2.sh

--- jenkins/script/scripted_pipeline_ex_2.sh
+++ jenkins/script/scripted_pipeline_ex_2.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-some_text="Function to conversion of excel sheet into Data frame"
-echo $some_text
 
-cat <<EOF | python3 -
-# import pandas lib as pd 
-import pandas as pd
-import xml.etree.cElementTree as ET
-import requests
- 
-# read by default 1st sheet of an excel file
-df1 = pd.read_excel (r'./jenkins/Individual_parameter_coverage.xlsx')
-print(df1)
-xml_data = open(r'./jenkins/config.xml').read()  # Read file
-
-EOF
