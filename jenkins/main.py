7dbe2cfaf70898c84a81097fb10f7d66da9f80e5
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-28 10:23:04 +0530

    Update main.py

--- jenkins/main.py
+++ jenkins/main.py
@@ -1,8 +1,7 @@
-
-# import pandas lib as pd
 import pandas as pd
+import xml.etree.cElementTree as ET
+import requests
  
 # read by default 1st sheet of an excel file
-dataframe1 = pd.read_excel('Individual_parameter_coverage.xlsx')
- 
-print(dataframe1)
+df1 = pd.read_excel (r'./jenkins/Individual_parameter_coverage.xlsx')
+print(df1)
